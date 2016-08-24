96b72bd488acdf4a95cea38be3f7ca098fa5bff1
BornToBeKilled <dexter262144@gmail.com>
2016-08-24 03:51:11 +0300

    create form

---
+++ GOL/src/Const.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Alex on 24.08.2016.
+ */
+public interface Const {
+    String NAME = "Game of Life";
+
+    int LOCATION_X = 100;
+    int LOCATION_Y = 100;
+    int SIZE_X = 400;
+    int SIZE_Y = 400;
+}
--- GOL/src/GOL.java
+++ GOL/src/GOL.java
@@ -1,8 +1,21 @@
 /**
  * Created by Alex on 24.08.2016.
  */
+import javax.swing.*;
+
+
 public class GOL {
+    private JFrame jFrame;
+
+    private void go(){
+        jFrame = new JFrame(Const.NAME);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setLocation(Const.LOCATION_X, Const.LOCATION_Y);
+        jFrame.setSize(Const.SIZE_X, Const.SIZE_Y);
+        jFrame.setVisible(true);
+    }
+
     public static void main(String[] args){
-        ;
+        new GOL().go();
     }
 }
