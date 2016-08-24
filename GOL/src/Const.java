69012bb0aab721e3ae84aa9edcd412ddd8af3185
BornToBeKilled <dexter262144@gmail.com>
2016-08-24 06:10:26 +0300

    add field drawing

--- GOL/src/Const.java
+++ GOL/src/Const.java
@@ -6,10 +6,11 @@ public interface Const {
 
     int LOCATION_X = 100;
     int LOCATION_Y = 100;
-    int RADIUS = 15;
-    int FIELD_SIZE_X = 50;
-    int FIELD_SIZE_Y = 50;
+    int DIAMETER = 15;
+    int FIELD_SIZE_X = 40;
+    int FIELD_SIZE_Y = 40;
     int BTN_PANEL_HEIGHT = 65;
-    int SIZE_X = FIELD_SIZE_X * RADIUS + 10;
-    int SIZE_Y = FIELD_SIZE_Y * RADIUS + 10 + BTN_PANEL_HEIGHT;
+    int OFFSET = 10;
+    int SIZE_X = FIELD_SIZE_X * DIAMETER + DIAMETER + OFFSET;
+    int SIZE_Y = FIELD_SIZE_Y * DIAMETER + DIAMETER + BTN_PANEL_HEIGHT + OFFSET;
 }
--- GOL/src/GOL.java
+++ GOL/src/GOL.java
@@ -1,32 +1,34 @@
 /**
  * Created by Alex on 24.08.2016.
  */
+
+import java.awt.*;
 import javax.swing.*;
 import java.util.Random;
 
 
 public class GOL {
-    enum Point{
-        Blank,
-        Filled,
-        Create,
-        Destroy
-    }
 
     private Random random = new Random();
 
     private JFrame jFrame;
-
-    private Point[][] field;
+    private Canvas canvasPanel;
+    private boolean[][] field;
 
     private void go(){
         jFrame = new JFrame(Const.NAME);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.setLocation(Const.LOCATION_X, Const.LOCATION_Y);
         jFrame.setSize(Const.SIZE_X, Const.SIZE_Y);
+
+        canvasPanel = new Canvas();
+        canvasPanel.setBackground(new Color(200, 200, 200));
+
+        jFrame.getContentPane().add(BorderLayout.CENTER, canvasPanel);
         jFrame.setVisible(true);
 
         FillField();
+        canvasPanel.repaint();
     }
 
     public static void main(String[] args){
@@ -34,11 +36,31 @@ public class GOL {
     }
 
     private void FillField(){
-        field = new Point[Const.FIELD_SIZE_Y][Const.FIELD_SIZE_X];
+        field = new boolean[Const.FIELD_SIZE_Y][Const.FIELD_SIZE_X];
         for(int i = 0; i < Const.FIELD_SIZE_Y; i++) {
             for (int j = 0; j < Const.FIELD_SIZE_X; j++) {
-                field[i][j] = (random.nextBoolean()) ? Point.Filled : Point.Blank;
+                field[i][j] = random.nextBoolean();
             }
         }
     }
+
+    private class Canvas extends JPanel {
+
+        @Override
+        public void paint(Graphics g) {
+            super.paint(g);
+            g.setColor(new Color(0, 100, 0));
+            for(int i = 0; i < Const.FIELD_SIZE_Y; i++) {
+                for (int j = 0; j < Const.FIELD_SIZE_X; j++) {
+                    if(field[i][j]){
+                        g.fillOval( Const.OFFSET / 2 + j * Const.DIAMETER,
+                                Const.OFFSET / 2 + i * Const.DIAMETER,
+                                Const.DIAMETER,
+                                Const.DIAMETER);
+                    }
+                }
+            }
+        }
+    }
+
 }
