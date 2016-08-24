12c6dedb148a45dd375ba6a06f886c0151ed7dfc
BornToBeKilled <dexter262144@gmail.com>
2016-08-24 05:20:46 +0300

    add fill field

--- GOL/src/Const.java
+++ GOL/src/Const.java
@@ -6,6 +6,10 @@ public interface Const {
 
     int LOCATION_X = 100;
     int LOCATION_Y = 100;
-    int SIZE_X = 400;
-    int SIZE_Y = 400;
+    int RADIUS = 15;
+    int FIELD_SIZE_X = 50;
+    int FIELD_SIZE_Y = 50;
+    int BTN_PANEL_HEIGHT = 65;
+    int SIZE_X = FIELD_SIZE_X * RADIUS + 10;
+    int SIZE_Y = FIELD_SIZE_Y * RADIUS + 10 + BTN_PANEL_HEIGHT;
 }
--- GOL/src/GOL.java
+++ GOL/src/GOL.java
@@ -2,10 +2,22 @@
  * Created by Alex on 24.08.2016.
  */
 import javax.swing.*;
+import java.util.Random;
 
 
 public class GOL {
+    enum Point{
+        Blank,
+        Filled,
+        Create,
+        Destroy
+    }
+
+    private Random random = new Random();
+
     private JFrame jFrame;
+
+    private Point[][] field;
 
     private void go(){
         jFrame = new JFrame(Const.NAME);
@@ -13,9 +25,20 @@ public class GOL {
         jFrame.setLocation(Const.LOCATION_X, Const.LOCATION_Y);
         jFrame.setSize(Const.SIZE_X, Const.SIZE_Y);
         jFrame.setVisible(true);
+
+        FillField();
     }
 
     public static void main(String[] args){
         new GOL().go();
+    }
+
+    private void FillField(){
+        field = new Point[Const.FIELD_SIZE_Y][Const.FIELD_SIZE_X];
+        for(int i = 0; i < Const.FIELD_SIZE_Y; i++) {
+            for (int j = 0; j < Const.FIELD_SIZE_X; j++) {
+                field[i][j] = (random.nextBoolean()) ? Point.Filled : Point.Blank;
+            }
+        }
     }
 }
